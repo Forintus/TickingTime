9b6d5e680c99f39131f57c78f70acacee83ab985
Meindert Kuyvenhoven <meindertkuyvenhoven@MacBook-Air-van-Meindert.local>
2018-01-07 14:22:18 +0100

    Update Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
+
+  private interval: any;
+  private time: Date;
+  private date: string;
+  private seconds: string;
 
   constructor(public navCtrl: NavController) {
-
+    this.interval = setInterval(() => {
+      let date = new Date();
+      this.seconds = date.getSeconds() / 60 * 100 + "%";
+      this.time = new Date(); 
+      const options = { 
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric' 
+      };
+      // let language = 'fr-FR';
+      let language = navigator.language;
+      this.date = date.toLocaleString(language, options);
+    }, 1000);
   }
 
+  ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
 }
